9e2d9b30627a0806f84241b4b8c864bae69313f3
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-06-21 16:47:42 +0200

    test tjr bloquant

--- src/main/java/dev/config/ServicesConfig.java
+++ src/main/java/dev/config/ServicesConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-
 @ComponentScan({"dev.repository"})
 public class ServicesConfig {
 }
--- src/main/java/dev/repository/CarRepository.java
+++ src/main/java/dev/repository/CarRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
-@CrossOrigin(origins = "http://localhost:4200")
 public interface CarRepository extends JpaRepository<Car,Integer> {
 }
