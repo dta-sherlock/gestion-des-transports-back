799b35305ffbb55aa66bde9e4e6874b6ce408b8a
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-22 14:16:07 +0200

    Création classe test Booking

---
+++ src/test/dev/repository/BookingRepositoryTest.java
@@ -0,0 +1,25 @@
+package dev.repository;
+
+import dev.services.DataInitService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ComponentScan(basePackages = "dev.config")
+@Profile("dev-mysql")
+public class BookingRepositoryTest {
+
+    @Autowired
+    private BookingRepository bookingRepo;
+
+    @Test
+    public void testBooking() {
+        
+    }
+}
