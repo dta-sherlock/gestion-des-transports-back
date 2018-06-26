d228f1b4c55a0209d8b58f5fb9f45b1ac5e8f668
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-26 12:27:20 +0200

    Ajout méthodes post pour controller Booking

--- src/main/java/dev/controller/BookingController.java
+++ src/main/java/dev/controller/BookingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,10 +31,22 @@ public class BookingController {
 	public List<Booking> getBookings() {
 		return bookingRepo.findAll();
 	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	@Secured({})
+	public void saveBooking(@ModelAttribute("booking") Booking booking) {
+		bookingRepo.save(booking);
+	}
 
 	@RequestMapping(value = "/companyCarBookings", method = RequestMethod.GET)
 	@Secured({})
 	public List<CompanyCarBooking> getCompanyCarBookings() {
 		return companyCarBookingRepo.findAll();
+	}
+
+	@RequestMapping(value = "/companyCarBookings", method = RequestMethod.POST)
+	@Secured({})
+	public void saveCompanyCarBooking(@ModelAttribute("companyCarBooking") CompanyCarBooking companyCarBooking) {
+		companyCarBookingRepo.save(companyCarBooking);
 	}
 }
