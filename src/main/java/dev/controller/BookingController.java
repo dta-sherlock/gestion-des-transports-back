11ba9659fb365515e8b1b8e3fe3acf8c1a6f4cc6
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-26 12:15:07 +0200

    Ajout méthodes controller pour lister les réservations

--- src/main/java/dev/controller/BookingController.java
+++ src/main/java/dev/controller/BookingController.java
@@ -1,15 +1,39 @@
 package dev.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.entity.Booking;
 import dev.entity.CompanyCarBooking;
+import dev.repository.BookingRepository;
+import dev.repository.CompanyCarBookingRepository;
 
 @RestController
-@RequestMapping("/reservations")
+@RequestMapping("/bookings")
 @CrossOrigin
 public class BookingController {
-	
+
+	@Autowired
+	BookingRepository bookingRepo;
+
+	@Autowired
+	CompanyCarBookingRepository companyCarBookingRepo;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	@Secured({})
+	public List<Booking> getBookings() {
+		return bookingRepo.findAll();
+	}
+
+	@RequestMapping(value = "/companyCarBookings", method = RequestMethod.GET)
+	@Secured({})
+	public List<CompanyCarBooking> getCompanyCarBookings() {
+		return companyCarBookingRepo.findAll();
+	}
 }
