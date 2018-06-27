24bf9cd5fbc88ed55666b1d98372456c12a7224d
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-27 13:07:51 +0200

    Ajout méthodes get et post pour enregistrer réservation

--- src/main/java/dev/controller/BookingController.java
+++ src/main/java/dev/controller/BookingController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.entity.CompanyCarBooking;
+import dev.entity.User;
 import dev.repository.CompanyCarBookingRepository;
+import dev.repository.UserRepository;
 
 @RestController
 @RequestMapping("/collaborateur/reservations")
@@ -20,11 +23,21 @@ public class BookingController {
 
 	@Autowired
 	CompanyCarBookingRepository companyCarBookingRepo;
+	
+	@Autowired
+	UserRepository userRepo;
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	@Secured({})
-	public List<CompanyCarBooking> getCompanyCarBookings() {
+	public List<CompanyCarBooking> listCompanyCarBookings() {
 		return companyCarBookingRepo.findAll();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@Secured({})
+	public List<CompanyCarBooking> listCompanyCarBookingsByCreator(@PathVariable int id) {
+		User creator = userRepo.findOne(id);
+		return companyCarBookingRepo.findByCreator(creator);
 	}
 
 	@RequestMapping(value = "/creer", method = RequestMethod.POST)
--- src/main/java/dev/repository/CompanyCarBookingRepository.java
+++ src/main/java/dev/repository/CompanyCarBookingRepository.java
@@ -1,7 +1,11 @@
 package dev.repository;
 
 import dev.entity.CompanyCarBooking;
+import dev.entity.User;
+
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyCarBookingRepository extends JpaRepository<CompanyCarBooking, Integer> {
+	List<CompanyCarBooking> findByCreator(User creator);
 }
